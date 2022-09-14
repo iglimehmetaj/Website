4a60706a11073158f550e4bdbddb46c75041cec6
iglimehmetaj <111540256+iglimehmetaj@users.noreply.github.com>
2022-09-14 19:05:56 +0200

    Add files via upload

---
+++ productScript.js
@@ -0,0 +1,20 @@
+var mainimg=document.getElementById("mainimg");
+var smallimg1=document.getElementById("small-img1");
+var smallimg2=document.getElementById("small-img2");
+var smallimg3=document.getElementById("small-img3");
+var smallimg4=document.getElementById("small-img4");
+
+
+
+smallimg1.onclick=function(){
+    mainimg.src=smallimg1.src;
+}
+smallimg2.onclick=function(){
+    mainimg.src=smallimg2.src;
+}
+smallimg3.onclick=function(){
+    mainimg.src=smallimg3.src;
+}
+smallimg4.onclick=function(){
+    mainimg.src=smallimg4.src;
+}
